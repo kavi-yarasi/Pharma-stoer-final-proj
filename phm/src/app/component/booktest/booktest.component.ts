cbe9f9997b9745d3d6a7e606bcf9f25a05abd4fe
Kaviyarasi <lkaviyarasik4@gmail.com>
2022-09-21 11:22:53 +0530

    booking test

--- phm/src/app/component/booktest/booktest.component.ts
+++ phm/src/app/component/booktest/booktest.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup,FormControl,ReactiveFormsModule} from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { LoginservService } from 'src/app/service/loginserv.service';
 @Component({
   selector: 'app-booktest',
@@ -8,38 +8,45 @@ import { LoginservService } from 'src/app/service/loginserv.service';
   styleUrls: ['./booktest.component.css']
 })
 export class BooktestComponent implements OnInit {
-  test:any= [];
+  test: any = [];
   testsel!: Number;
   txt1!: string;
-  vall=this.serv.vall;
-  dta=this.serv.values;
-  constructor(public serv:LoginservService,public router:Router) { 
-    
+  vall = this.serv.vall;
+  dta = this.serv.values;
+  nm: any
+  constructor(public serv: LoginservService, public router: Router) {
+    console.log(this.testsel);
+
   }
   ngOnInit() {
-    this.test=[{Id:1,Test:"Select Lab tests"},
-      {Id:2,Test:"LFT"},
-      {Id:3,Test:"Insulin"},
-      {Id:4,Test:"Ferritin"},
-      {Id:5,Test:"Potassium serum Plasma"},
-      {Id:6,Test:"Chloesterol"},
-      {Id:7,Test:"Platelet count"}
+    this.test = [{ Id: 1, Test: "Select Lab tests" },
+    { Id: 2, Test: "LFT" },
+    { Id: 3, Test: "Insulin" },
+    { Id: 4, Test: "Ferritin" },
+    { Id: 5, Test: "Potassium serum Plasma" },
+    { Id: 6, Test: "Chloesterol" },
+    { Id: 7, Test: "Platelet count" }
     ];
-    this.testsel=1;
-    console.log(this.testsel)
+
   }
-  fnc1(){
+  fnc1() {
     console.log(this.vall.value);
-    this.dta.v1=JSON.stringify(this.vall.value.v1);
-    this.dta.v2=JSON.stringify(this.vall.value.v2);
-    this.dta.v3=JSON.stringify(this.vall.value.v3);
-    
+    this.dta.v1 = JSON.stringify(this.vall.value.v1);
+    this.dta.v2 = JSON.stringify(this.vall.value.v2);
+    this.dta.v3 = JSON.stringify(this.vall.value.v3);
+    console.log();
+    this.nm = this.testsel
+    console.log();
+    this.dta.v4 = JSON.stringify(this.test[this.nm - 1].Test);
+
+
+
     this.router.navigate(['booktesdis']);
   }
 
-  ontestsel(val:any){
-   return (val)
+  ontestsel(val: any) {
+    return (val)
   }
-  
+
 
 }
